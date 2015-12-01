5e15653ae0a36ef7246689973d07e3fe5a980d2d
DavidTsui <st880221@gmail.com>
2015-12-01 09:53:23 +0800

    增加身份驗證、修改資料功能(限人名皆不同)

--- appscript.js
+++ appscript.js
@@ -23,6 +23,14 @@ function doPost(e) {
 }
 
 function handleResponse(e, type, check) {
+  /*e = {
+    姓名: "崔家華",
+    信箱: "st880221@gmail.com",
+  Avatar: 123,
+  Github: 321,
+  Demopage: 123,
+  Info: "test"
+  };*/
   // shortly after my original solution Google announced the LockService[1]
   // this prevents concurrent access overwritting data
   // [1] http://googleappsdeveloper.blogspot.co.uk/2011/10/concurrency-and-google-apps-script.html
@@ -52,33 +60,64 @@ function handleResponse(e, type, check) {
         })
         ret.push(temp);       
       })
-  
-      // return json success results
       return (
         ContentService.createTextOutput(JSON.stringify({"result":"success", "type": type, "row": nextRow, "output": ret}))
         .setMimeType(ContentService.MimeType.JSON)
       );
     }
     else if (type == "post") {
-      // loop through the header columns
-      headers.forEach(function(header) {
-        if (header == "Timestamp"){ // special case if you include a 'Timestamp' column
-          ret.push(new Date());
-        } else { // else use header name to get data
-          ret.push(e.parameter[header]);
+      var obj = e.parameter;
+      var arr = [];
+      if (CheckPostIsActualModify(obj) != -1) {
+        var ret_row = CheckPostIsActualModify(obj);
+        var email_column = getEmailColumn();
+        if (checkInfoCorrespond(ret_row, email_column, obj["信箱"])) {
+          for(var key in obj) {
+            for(var i = 0; i < headers.length; i++) {
+              if (key == headers[i]) {
+                arr.push(i + 1);
+                break;
+              }
+            }
+          }
+          var keys = Object.keys(obj);
+          for(var i = 0; i < keys.length; i++) {
+            var key = keys[i];
+            sheet.getRange(temp_row, arr[i], 1, 1).setValue(obj[key]);
+          }
+          return (
+            ContentService.createTextOutput(JSON.stringify({"result":"success", "type": "modify"}))
+            .setMimeType(ContentService.MimeType.JSON)
+          );
         }
-      })
-      // more efficient to set values as [][] array than individually
-      sheet.getRange(nextRow, 1, 1, ret.length).setValues([ret]); // insert the data
-      // return json success results
-      return (
-        ContentService.createTextOutput(JSON.stringify({"result":"success", "type": type, "row": nextRow, "insert": ret}))
-        .setMimeType(ContentService.MimeType.JSON)
-      );
+        else {
+          return (
+            ContentService.createTextOutput(JSON.stringify({"result":"error", "type": "modify", "reason": "Authentication failed"}))
+            .setMimeType(ContentService.MimeType.JSON)
+          );
+        }
+      }
+      else {   
+        // loop through the header columns
+        headers.forEach(function(header) {
+          if (header == "Timestamp"){ // special case if you include a 'Timestamp' column
+            ret.push(new Date());
+          } else { // else use header name to get data
+            ret.push(e.parameter[header]);
+          }
+        })
+        // more efficient to set values as [][] array than individually
+        sheet.getRange(nextRow, 1, 1, ret.length).setValues([ret]); // insert the data
+        // return json success results
+        return (
+          ContentService.createTextOutput(JSON.stringify({"result":"success", "type": type, "row": nextRow, "insert": ret}))
+          .setMimeType(ContentService.MimeType.JSON)
+        );
+      }
     }
     else {
       return (
-        ContentService.createTextOutput(JSON.stringify({"result":"error", "type": type, "reason": "no authority"}))
+        ContentService.createTextOutput(JSON.stringify({"result":"error", "type": type, "reason": "Permission denied"}))
         .setMimeType(ContentService.MimeType.JSON)
       ); 
     }
@@ -95,4 +134,60 @@ function handleResponse(e, type, check) {
 function setup() {
   var doc = SpreadsheetApp.getActiveSpreadsheet();
   SCRIPT_PROP.setProperty("key", doc.getId());
+}
+
+
+function CheckPostIsActualModify(obj) {
+  /*obj = {
+    姓名: "崔家華",
+    信箱: "st880221@gmail.com",
+  Avatar: 123,
+  Github: 321,
+  Demopage: 123,
+  Info: "test"
+  };*/
+  var doc = SpreadsheetApp.openById(SCRIPT_PROP.getProperty("key"));
+  var sheet = doc.getSheetByName(SHEET_NAME);
+  var rows = sheet.getLastRow();
+  var columns = sheet.getLastColumn();
+  var headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0];
+  var name_column = getNameColumn();
+  
+  for(var i = 2; i <= rows; i++) {
+    var value = sheet.getRange(i, name_column, 1, 1).getValue();
+    if (value == obj["姓名"]) {
+      return i;
+    }
+  }
+  return -1; // 找不到
+}
+
+function checkInfoCorrespond(row, email_column, email) {
+  var doc = SpreadsheetApp.openById(SCRIPT_PROP.getProperty("key"));
+  var sheet = doc.getSheetByName(SHEET_NAME);
+  var true_email = sheet.getRange(row, email_column, 1, 1).getValue();
+  if (true_email == email)
+    return true;
+  else
+    return false;
+}
+
+function getNameColumn() {
+  var doc = SpreadsheetApp.openById(SCRIPT_PROP.getProperty("key"));
+  var sheet = doc.getSheetByName(SHEET_NAME);
+  var headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0];
+  for(var i = 0; i < headers.length; i++) {
+    if (headers[i] == "姓名") return i + 1;
+  }
+  return -1;
+}
+
+function getEmailColumn() {
+   var doc = SpreadsheetApp.openById(SCRIPT_PROP.getProperty("key"));
+  var sheet = doc.getSheetByName(SHEET_NAME);
+  var headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0];
+  for(var i = 0; i < headers.length; i++) {
+    if (headers[i] == "信箱") return i + 1;
+  }
+  return -1;
 }
